b0f5926577296b2482e95678542efda4d25978e4
ariwilyan <36038264+ariwilyan@users.noreply.github.com>
2018-02-01 12:38:47 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,22 @@
+a = 20
+b = 25
+print(a)
+hasil = a*b
+print(hasil)
+
+if a==60 :
+	print ("False")
+else:
+	print("True")
+
+list = [60,34,67,46,24,5,9,1]
+print(list)
+
+list.sort()
+print(list)
+
+list.reverse()
+print(list)
+
+list.remove(60)
+print(list)
